cb05615feae1e5ea675a4e8dee16bf1d1d63c265
Wyatt Brege <wyatt.brege@wsu.edu>
2016-07-29 19:42:20 -0400

    Add scripts to produce density/temp at latest time
    This script is still a work in progress.  Right now
    I've gotten it to calculate the corresponding
    HyDomainAtTime*.txt file for the latest
    VolumeMatterData/*.h5 files available.
    
    I got pretty far into it until I realized bash
    couldn't do arithmetic of two decimal numbers, so we
    wrote a python script to do that in its place.  I'd
    probably just want to do this all in python if I
    started again..

---
+++ match-domain-time-to-pv-file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys, os
+import numpy as np
+sys.path.insert(0,os.path.realpath(__file__+'/../../Python'))
+
+hytimes = np.loadtxt('/RQexec/brege/MicrophysicsSurvey/BHNS/tmp/hytimes.txt',delimiter=' ')
+h5time = np.loadtxt('/RQexec/brege/MicrophysicsSurvey/BHNS/tmp/h5time.txt',delimiter=' ')
+
+#print h5time
+
+biggest=10000000
+tol = 0.000001
+
+for t in range(len(hytimes)):
+    smallest = abs(hytimes[t] - h5time)
+    if smallest < biggest and hytimes[t] - h5time < 0.:
+        biggest = smallest
+
+#print biggest
+
+for t in range(len(hytimes)):
+    if abs(hytimes[t] - h5time)-biggest < tol and hytimes[t] - h5time < 0.:
+        print('%.8f' % hytimes[t])
---
+++ volumegenerater.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# usage:
+# ./volumegenerater.sh Lev0 Lev1 Lev2
+
+set -e
+set -u
+shopt -s nullglob
+
+while [ $# -ge 1 ] ; do
+        lev=$1
+#	lev="Lev1"
+        basedir="/RQexec/brege/MicrophysicsSurvey/BHNS"
+        sdbase="${basedir}/*/M1?_7-S9-*/QE/Ev-eqsym/*AMR${lev}_Plunge/${lev}_SettleDisk"
+        shift
+
+	echo `date`
+	for run in $sdbase ; do
+		cd $run
+		echo "================================================================"
+		echo "Descending into $run"
+
+		seg="$(ls --ignore '*.*' | grep ${lev}_ | sort -n | tail --lines 1)"
+		vmdir="${run}/${seg}/Run/VolumeMatterData"
+		if [ -d $vmdir ]
+		then
+
+			echo "Candidate vmdir is: " $vmdir
+			lastH5="$(ls ${vmdir} | grep -i "Vars_Interval" | tail --line 1)"
+			echo $lastH5
+
+#			if [ ! -f $vmdir/$lastH5 ] ;
+#                	then
+#				echo "We didn't find any h5 files in the latest segment.. " $seg
+#				seg="$(ls --ignore '*.*' | grep ${lev}_ | sort -n | tail --lines $i+1 | head --lines 1)" 
+#				echo "Falling back to last segment" $seg
+#				vmdir="${run}/${seg}/Run/VolumeMatterData"
+#				lastH5="$(ls $vmdir | grep -i "Vars_Interval" | tail --line 1)"
+#			fi
+			h=1
+			while [ ! -f $vmdir/$lastH5 ] ;
+			do
+				h=$((h+1))
+				echo "We didn't find any h5 files in" $seg
+				seg="$(ls --ignore '*.*' | grep ${lev}_ | sort -n | tail --lines $h | head --lines 1)" 
+				echo "Falling back to last segment" $seg
+				vmdir="${run}/${seg}/Run/VolumeMatterData"
+				lastH5="$(ls ${vmdir} | grep -i "Vars_Interval" | tail --line 1)"
+			done
+
+			lasth5=$(ls -tr ${vmdir}/Vars_Interval*.h5 | tail --lines 1)
+			echo "Latest written H5 file is: " $lasth5
+			h5time=$(/RQusagers/brege/SpEC/Support/bin/TimesInH5File ${lasth5} | awk -F" " '{print $4}'  | tail --lines 1) 
+#			echo "The latest time is: " $h5time
+			hytimes=$(ls ${run}/${seg}/Run/HyDomainAtTime*.txt | awk -F"    " '{print $2}' | awk -F".txt" '{print $1}')
+#			echo "The domain times are: " $hytimes
+
+			echo $h5time 1> "${basedir}/tmp/h5time.txt"
+			echo $hytimes 1> "${basedir}/tmp/hytimes.txt"
+
+			hytime=$(python $basedir/scripts/"match-domain-time-to-pv-file.py")
+			echo "hytime is: " $hytime
+
+			OLDIFS=$IFS
+			IFS=$'\n'
+			hydomain="${run}/${seg}/Run/HyDomainAtTime-    ${hytime}.txt"
+			echo "hydomain: " $hydomain 
+			echo " "  
+			IFS=$OLDIFS
+
+		fi
+	done
+
+done
