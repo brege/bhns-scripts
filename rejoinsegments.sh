7fca152d8c634cee2714289b8a2e3c6045840e6d
Wyatt Brege <wyatt.brege@wsu.edu>
2016-09-28 20:39:04 -0400

    Fix small bugs in segment joiner

--- rejoinsegments.sh
+++ rejoinsegments.sh
@@ -17,16 +17,16 @@ for run in $sdbase ; do
 	cd $run
 	lastseg="$(ls --ignore '*.*' | grep ${lev}_ | sort -n | tail --lines 1)"
 	echo "Descending into " $run
-	if [ -d $run/$lastseg/Run ] ;
+	if [ -d "${run}/${lastseg}/Run" ] ;
 	then
 		if [ -d JoinedLev1 ] ;
 		then 
-			echo "Removingi old joined data since the directory already exists.."
+			echo "Removing old joined data since the directory already exists.."
 			rm -r ./JoinedLev1
 		else
 			echo "No joined data here.  Proceeding without deleting anything.."
 		fi
-		/home/brege/SpEC/Support/bin/CombineSegments.py -L 1 -o JoinedLev1 -f Constraints MatterObservers TStepperDiag.dat TimeInfo.dat
+		/RQusagers/brege/SpEC/Support/bin/CombineSegments.py -L 1 -o JoinedLev1 -f Constraints MatterObservers Spin_AhA.dat TStepperDiag.dat TimeInfo.dat 
 		echo "Data successfully joined!"
 	else 
 		echo "Hmm," $run/$lastseg "hasn't run anything yet.  Skipping this for now.."
