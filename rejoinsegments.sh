59587d3fd5e8374e7b37a11649b2db19f92d4ac8
Wyatt Brege <wyatt.brege@wsu.edu>
2016-10-24 19:06:05 -0400

    Safely overwrite JoinedLev1 files
    Since other scripts might want to store information
    in the JoinedLev1/ directories, do not remove the
    directory if it exists.  Create a temporary directory
    with CombineSegments.py instead, then overwrite the
    old joined data with the new joined data.

--- rejoinsegments.sh
+++ rejoinsegments.sh
@@ -17,19 +17,29 @@ echo `date`
 #for run in $plbase ; do 
 for run in $sdbase ; do 
 	cd $run
+	echo "Descending into " $run
 #	lastseg="$(ls --ignore '*.*' --ignore '*SettleDisk' | grep ${lev}_ | sort -n | tail --lines 1)"
 	lastseg="$(ls --ignore '*.*' | grep ${lev}_ | sort -n | tail --lines 1)"
-	echo "Descending into " $run
 	if [ -d "${run}/${lastseg}/Run" ] ;
 	then
+		# Make sure to remove (failed) partial tmp dirs just in case
+		if [ -d JoinedLev1-tmp ] ;
+		then
+			echo "Removing old temporary directory"
+			rm -r ./JoinedLev1-tmp/
+		fi
+
 		if [ -d JoinedLev1 ] ;
 		then 
-			echo "Removing old joined data since the directory already exists.."
-			rm -r ./JoinedLev1
+			echo "Overwriting old joined data since the directory already exists.."
+		#	rm -r ./JoinedLev1/
+			/RQusagers/brege/SpEC/Support/bin/CombineSegments.py -e dat -L 1 -o JoinedLev1-tmp -f Constraints MatterObservers ApparentHorizon TStepperDiag.dat TimeInfo.dat
+			cp -r JoinedLev1-tmp/* JoinedLev1/
+			rm -r ./JoinedLev1-tmp/
 		else
-			echo "No joined data here.  Proceeding without deleting anything.."
+			echo "No joined data here.  Proceeding without overwriting anything.."
+			/RQusagers/brege/SpEC/Support/bin/CombineSegments.py -e dat -L 1 -o JoinedLev1 -f Constraints MatterObservers ApparentHorizon TStepperDiag.dat TimeInfo.dat 
 		fi
-		/RQusagers/brege/SpEC/Support/bin/CombineSegments.py -L 1 -o JoinedLev1 -f Constraints MatterObservers GridAhA.dat Spin_AhA.dat TStepperDiag.dat TimeInfo.dat 
 		echo "Data successfully joined!"
 	else 
 		echo "Hmm," $run/$lastseg "hasn't run anything yet.  Skipping this for now.."
