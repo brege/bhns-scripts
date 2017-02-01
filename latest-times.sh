656f55d37f5d79c4933c577dae3e2a7246777bd6
Wyatt Brege <wyatt.brege@wsu.edu>
2017-02-01 17:54:05 -0500

    Rewrite for readability, loop for change in res'n
    May want to put the "levs=..." in the conf file
    in the future

--- latest-times.sh
+++ latest-times.sh
@@ -7,35 +7,68 @@ set -e
 set -u
 shopt -s nullglob
 
-while [ $# -ge 1 ] ; do
-        lev=$1
-        basedir="/RQexec/brege/MicrophysicsSurvey/BHNS"
-        sdbase="${basedir}/*/M1?_7-S9-*/QE/Ev-eqsym/*AMR${lev}_Plunge/${lev}_SettleDisk"
-#	sdbase="${basedir}/*/M1?_7-S9-*/QE/Ev-eqsym/*AMR${lev}_Plunge/"
-        shift
+#basedir="/RQexec/brege/MicrophysicsSurvey/BHNS"
+#sdbase="${basedir}/*/M1?_7-S9-*/QE/Ev-eqsym/*AMRLev1_Plunge/Lev?_SettleDisk"
+#sdbase="${basedir}/*/M1?_7-S9-*/QE/Ev-eqsym/*AMR${lev}_Plunge/"
+source ./paths.conf
 
-	echo `date`
-	echo -e "Simulation\tCores\tSubds\tLevels\tSegment\tTime"
+echo `date`
+echo -e "Simulation\tCores\tSubds\tLevels\tSegment\tTime"
 
-	for run in $sdbase ; do 
-		cd $run
-                seg="$(ls --ignore '*.*' | grep ${lev}_ | sort -n | tail --lines 1)"
+for run in $sdbase ; do 
+	cd $run
+	levs="0 1 5"
+	for level in $levs; do
+
+                lastseg="$(ls --ignore '*.*' | grep "Lev${level}_" | sort -n | tail --lines 1)"
+                if [ ! -d "${lastseg}" ] ;
+                then
+#                        echo "no available Lev${level} directory here, skipping.."
+                        continue
+                fi
+
+
+                seg="$(ls --ignore '*.*' \
+                       | grep "Lev${level}_" \
+                       | sort -n \
+                       | tail --lines 1)"
 		if [ ! -f "${run}/${seg}/Run/TStepperDiag.dat" ] ;
 		then
-			seg="$(ls --ignore '*.*' | grep ${lev}_ | sort -n | tail --lines 2 | head --lines 1)"
+			seg="$(ls --ignore '*.*' \
+                               | grep "Lev${level}_" \
+                               | sort -n \
+                               | tail --lines 2 \
+                               | head --lines 1)"
 		fi
-		jobname=$(grep -i "Jobname" "${run}/${seg}/Run/MakeSubmit.input" | awk -F" " '{print $3}')
-		cores=$(grep -i 'Cores\ =\ ' "${run}/${seg}/Run/MakeSubmit.input" | awk -F" " '{print $3}' | sed -n -e '2{p;q}' )
-		latesttime=$(tail -n 1 "${run}/${seg}/Run/TStepperDiag.dat" | awk -F" " '{print $1}')
+
+		jobname=$(grep -i "Jobname" "${run}/${seg}/Run/MakeSubmit.input" \
+                        | awk -F" " '{print $3}')
+                cores=$(grep -i 'Cores\ =\ ' "${run}/${seg}/Run/MakeSubmit.input" \
+                        | awk -F" " '{print $3}' \
+                        | sed -n -e '2{p;q}' )
+		latesttime=$(tail -n 1 "${run}/${seg}/Run/TStepperDiag.dat" \
+                             | awk -F" " '{print $1}')
+
 		if [ ! -f "${run}/${seg}/Run/NextHyDomain.input" ]
 		then
-	                levels=$(cat "${run}/${seg}/Run/HyDomain.input" | grep "BaseName = IntervalB-Lev" | wc -l )
-			subdomains=$(/RQusagers/brege/SpEC/Support/bin/DomainInfo -Nsubdomains -d "${run}/${seg}/Run/HyDomain.input" -IgnoreHist)
+	                levels=$(cat "${run}/${seg}/Run/HyDomain.input" \
+                                 | grep "BaseName = IntervalB-Lev" \
+                                 | wc -l )
+			subdomains=$(DomainInfo -Nsubdomains \
+                                                -d "${run}/${seg}/Run/HyDomain.input" \
+                                                -IgnoreHist)
 		else
-			levels=$(cat "${run}/${seg}/Run/NextHyDomain.input" | grep "BaseName = IntervalB-Lev" | wc -l )
-	#		levels=$(cat "${run}/${seg}/Run/NextHyDomain.input" | grep "BaseName = Interval-Lev" | wc -l )
-			subdomains=$(/RQusagers/brege/SpEC/Support/bin/DomainInfo -Nsubdomains -d "${run}/${seg}/Run/NextHyDomain.input" -IgnoreHist)
+			levels=$(cat "${run}/${seg}/Run/NextHyDomain.input" \
+                                 | grep "BaseName = IntervalB-Lev" \
+                                 | wc -l )
+#i			levels=$(cat "${run}/${seg}/Run/NextHyDomain.input" \
+#                                | grep "BaseName = Interval-Lev" \
+#                                | wc -l )
+			subdomains=$(DomainInfo -Nsubdomains \
+                                     -d "${run}/${seg}/Run/NextHyDomain.input" \
+                                     -IgnoreHist)
 		fi
+
 		echo -e $jobname"\t"$cores"\t"$subdomains"\t"$levels"\t"$seg"\t"$latesttime
 
 
