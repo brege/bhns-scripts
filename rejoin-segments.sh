30d11cf84c6dd695f7a11fd2d5dd36c79dabc004
Wyatt Brege <wyatt.brege@wsu.edu>
2017-05-23 18:26:12 -0400

    Allow user to manually skip joining segments
    If the file 'skip.txt' exists in a join directory (for instance a
    completed part of the run) do not rejoin those segments.

--- rejoin-segments.sh
+++ rejoin-segments.sh
@@ -14,47 +14,53 @@ for run in $sdbase ; do
 	cd $run
 	echo "Descending into " $run
 	for level in $levs; do
-		lastseg="$(ls --ignore '*.*' | grep -E "Lev${level}_[A-Z]{2,3}" | sort -n | tail --lines 1)"
+		lastseg="$(ls --ignore '*.*' \
+					| grep -E "Lev${level}_[A-Z]{2,3}" \
+					| sort -n \
+					| tail --lines 1)"
 
-		if [ ! -d "${lastseg}" ] ;
+		# If user adds a file skip.txt to join directory, do not join		
+		if [ -f "JoinedLev${level}/skip.txt" ]
 		then
-			echo "no available Lev${level} directory here, skipping.."
+			echo "File skip.txt exists. Skipping Lev${level} joining.."
 			continue
 		fi
 
-		if [ -d "${run}/${lastseg}/Run" ] ;
+		if [ ! -d "${lastseg}" ] ;
 		then
-			# Make sure to remove (failed) partial tmp dirs just in case
-			if [ -d "JoinedLev${level}-tmp" ] ;
-			then
-				echo "Removing old temporary directory"
-				rm -r "./JoinedLev${level}-tmp/"
-			fi
-	
-			if [ -d "JoinedLev${level}" ] ;
-			then 
-				echo "Overwriting old joined data since the directory already exists.."
-			#	rm -r ./JoinedLev1/
-				CombineSegments.py -e dat \
-								   -L $level \
-								   -o "JoinedLev${level}-tmp" \
-								   -f Constraints MatterObservers \
-									  ApparentHorizon TStepperDiag.dat \
-									  TimeInfo.dat MemoryInfo.dat
-				cp -r "JoinedLev${level}-tmp/"* "JoinedLev${level}/"
-				rm -r "./JoinedLev${level}-tmp/"
-			else
-				echo "No joined data here.	Proceeding without overwriting anything.."
-				CombineSegments.py -e dat \
-								   -L $level \
-								   -o "JoinedLev${level}" \
-								   -f Constraints MatterObservers \
-									  ApparentHorizon TStepperDiag.dat \
-									  TimeInfo.dat MemoryInfo.dat 
-			fi
-			echo "Data successfully joined!"
-		else 
-			echo "Hmm," $run/$lastseg "hasn't run anything yet.  Skipping this for now.."
+			echo "No available Lev${level} directory here, skipping.."
+			continue
+		else
+			echo "Joining Lev${level} directories.."
 		fi
+
+		# Make sure to remove (failed) partial tmp dirs just in case
+		if [ -d "JoinedLev${level}-tmp" ] ;
+		then
+			echo "Removing old temporary directory"
+			rm -r "./JoinedLev${level}-tmp/"
+		fi
+	
+		if [ -d "JoinedLev${level}" ] ;
+		then 
+			echo "Overwriting old joined data since the directory already exists.."
+			CombineSegments.py -e dat \
+							   -L $level \
+							   -o "JoinedLev${level}-tmp" \
+							   -f Constraints MatterObservers \
+								  ApparentHorizon TStepperDiag.dat \
+								  TimeInfo.dat MemoryInfo.dat
+			cp -r "JoinedLev${level}-tmp/"* "JoinedLev${level}/"
+			rm -r "./JoinedLev${level}-tmp/"
+		else
+			echo "No joined data here.	Proceeding without overwriting anything.."
+			CombineSegments.py -e dat \
+							   -L $level \
+							   -o "JoinedLev${level}" \
+							   -f Constraints MatterObservers \
+								  ApparentHorizon TStepperDiag.dat \
+								  TimeInfo.dat MemoryInfo.dat 
+		fi
+		echo "Data successfully joined!"
 	done
 done
